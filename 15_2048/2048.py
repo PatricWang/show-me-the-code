112c5dbb18a8d3ed874dcb751faadabd29e3b37f
Patrick Wang <wzhpanging@163.com>
2018-01-14 02:06:11 +0800

    2048
    完成，初始化和坐移操作

--- 15_2048/2048.py
+++ 15_2048/2048.py
@@ -1,11 +1,66 @@
-mat = [[i for i in range(4)] for j in range(4)]
-#print mat
+import random
 
-#for i in range(4):
-#print'{0:^6} {1:^6} {2:^6} {3:^6}'.format([mat[0][i] for i in range(4)])
 
-#str(mat[0][i]).center(6)
+mat = [[0 for row in range(4)] for col in range(4)]
+valid_usr_input = ('a', 'A', 'w', 'W', 's', 'S', 'd', 'D', 'r', 'q')
+# print mat
 
-def PrintMat():
+# for i in range(4):
+# print'{0:^6} {1:^6} {2:^6} {3:^6}'.format([mat[0][i] for i in range(4)])
+
+# str(mat[0][i]).center(6)
+
+
+def print_mat():
+
     for i in range(4):
-        print'{0:^6} {1:^6} {2:^6} {3:^6}'.format(mat[i][0],mat[i][1],mat[i][2],mat[i][3])
+        print'{0:^6} {1:^6} {2:^6} {3:^6}'.format(mat[i][0], mat[i][1], mat[i][2], mat[i][3])
+
+
+def new_num():
+    while True:
+        pos = random.randint(0, 15)
+        i = pos / 4
+        j = pos % 4
+        if not mat[0][j]:
+            mat[0][j] = random.choice([2, 4])
+            break
+    print 'pos: ', pos, 'i: ', i, 'j: ', j
+
+
+def receive_input():
+    while True:
+        usr_move_to = raw_input("move to:")
+        if usr_move_to in valid_usr_input:
+            break
+        else:
+            print 'need a valid input:'
+            move_left()
+            new_num()
+            print_mat()
+
+
+def move_left():
+    tmp = [0,0,0,0]
+    k = 0
+    for j in range(4):
+        if mat[0][j]:
+            tmp[k] = mat[0][j]
+            k += 1
+    j = 0
+    while j < len(tmp) - 1:  # [::-1]:
+        if tmp[j] == tmp[j+1]:
+            tmp[j] += tmp[j+1]
+            if j+2 < len(tmp):
+                tmp[j+1:-1] = tmp[j+2:]
+            # tmp[-1] = 0
+        j += 1
+    mat[0] = tmp[::]
+
+
+# new_num()
+mat[0] = [2,4,2,2] # 16 4 2 4
+print_mat()
+receive_input()
+# new_num()
+# print_mat()
--- 15_2048/test.py
+++ 15_2048/test.py
@@ -1,45 +1,19 @@
-import curses
-import string
+name = '123'
 
-stdscr = curses.initscr()
 
-def display_info(str,x=0,y=0,colorpair=1):
-    global stdscr
-    stdscr.addstr(y,x,str,curses.color_pair(colorpair))
-    stdscr.refresh()
+def f1():
+    print name
 
-def get_ch_and_continue():
-    global stdscr
-    stdscr.nodelay(0)
-    ch = stdscr.getch()
-    stdscr.nodelay(1)
-    return True
 
-def set_win():
-    global stdscr
-    curses.start_color()
-    curses.init_pair(1,curses.COLOR_GREEN,curses.COLOR_BLACK)
-    curses.init_pair(2,curses.COLOR_RED,curses.COLOR_BLACK)
-    curses.noecho()
-    curses.cbreak()
-    stdscr.nodelay(1)
+def f2():
+    name = '321'
+    return f1
 
-def unset_win():
-    global stdscr
-    curses.nocbreak()
-    curses.echo()
-    curses.endwin()
 
-if __name__ == '__main__':
-    try:
-        set_win()
-        display_info('hola,curses',1,1,2)
-        # for x in range(15):
-        #     for y in range(10):
-        #         display_info(string.ascii_letters[(5*x+y)%52],x,y,2)
-        display_info('press any key to continue..',0,9)
-        get_ch_and_continue()
-    except Exception,e:
-        raise e
-    finally:
-        unset_win()
+li = [lambda :x for x in range(10)]
+
+print li.__len__()
+print li[0]()
+print li[2]()
+# ret = f2()
+# ret()
